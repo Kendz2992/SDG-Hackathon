5fd0d16112e6943ff657e04b78269bb8205896d2
Chris Mera <developer.chrismera@gmail.com>
2020-06-20 18:32:31 -0400

    init front end with create-react-app. Establish fe file structure. Display map with hard coded data values

---
+++ app/http/web/app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import indexRoutes from "./routes/";
+import "./App.css";
+
+export default function App() {
+  return (
+    <Router basename="/">
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return (
+            <Route path={prop.path} key={key} component={prop.component} />
+          );
+        })}
+      </Switch>
+    </Router>
+  );
+}
---
+++ app/http/web/app/src/routes/index.js
@@ -0,0 +1,4 @@
+import Main from "../views/Main/Main";
+var indexRoutes = [{ path: "/", name: "Dashboard", component: Main }];
+
+export default indexRoutes;
